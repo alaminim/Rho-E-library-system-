3fc1a897dfd93c2a5b0ed143ed90c73db0b21ef4
Ibrahim Suleiman <83095347+iamkhaleel@users.noreply.github.com>
2021-07-07 20:51:02 -1200

    Login and Registration pages
    This is the login and registration page of the website

---
+++ Rho-E-Library/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+
+<head><title>RHO E-LIBARY </title></head>
+
+<body>
+  <div class="boxone">
+   <table  style =" font-size:25pt"  align="center" width="100%" height="100%" >
+      <tr>
+        <td style="color:#329299"><h1 align="center"><marquee> WELCOME TO RHO E-LIBRARY </marquee></td></h1>
+      </tr>
+      <tr>
+        <td style="color:#329299" align="center"> <b> LOGIN HERE </b></td>
+      </tr>
+    </table>
+  </div>
+    <br>
+    <br>
+
+    <form method="post" action="" id="lgin" >
+      <div  class="boxtwo" style="border:solid 4px #3d351d ;border-radius: 40px; width:73.5%; height:360px; margin-left:13%;margin-top:15px;background-color: black ; opacity: .68; background-repeat: no-repeat; background-size: cover; ">
+
+        <table style="font-size:25pt;width:300px;height:50px;margin-right: 310px; margin-top: 60px; position: relative; " align="center" >
+          <tr>
+            <td><br></td>
+          </tr>
+          <tr>
+            <td><label style="color:white ; font-size: 25px; ">Username:</label></td>
+            <td><input type="text" name="username" placeholder="username"  style="color:black; font-size: 25px; "></td>
+          </tr>
+
+          <tr>
+            <td><label style="color:white; font-size: 25px; ">Password:</label></td>
+            <td><input type="password" name="password" placeholder="**********"  style="color:black; font-size: 25px; "></td>
+          </tr>
+          <tr>
+            <td align="center" style="margin-left:200px"> <input type="submit" name="submit" value="LOGIN"></td>
+          </tr>
+          <tr>
+            <td style="font-size:10pt; color: white; top: 15px; position: relative;" > Not a Member !!!</td>
+
+
+            <td style="font-size:10pt"><a href="registration.php" style="color:white; font-style: bold; position: relative; top: 15px; ">Register Now</a></td>
+          </tr>
+          <tr>
+            <td ><br></td>
+            <td><br></td>
+          </tr>
+        </table>
+      </div >
+    </form>
+
+
+    <div align = center ; class="boxthree" style="background-color:black; border:solid 3px grey; border-radius: 6px; width:73.5%; height:40px; margin-left:13%; margin-top:20px" >
+      <h4 style="margin-top: 5px ; color : #329299 ; " >Thank You For Using This System.</h4>
+     </div>
+
+<Style>
+body {
+    background-image: url(alfons.jpg);
+}
+ .boxone{
+
+    width:74%;
+    margin-left: 13%;
+  }
+  .boxtwo{
+    background-size: cover;
+    box-shadow:0px 0px 15px #3d351d;
+    border-radius:12px;
+  }
+  input[type="submit"]
+  {
+
+      font-size:18px;
+      text-align:left;
+      border:none;
+      height:20px;
+      margin-left:97% ;
+      margin-top: 20px;
+      background:#3d351d;
+      color:#FFFFFF;
+      border-radius:10px;
+      }
+</Style>
