d3242c04f4457b1564cd43a1e860aa08c0d615bd
alaminim <71375779+alaminim@users.noreply.github.com>
2021-07-22 01:39:52 -0700

    added login confirmation

---
+++ Rho-E-Library/comp_SignUp.php
@@ -0,0 +1,45 @@
+<!Doctype html>
+<html>
+<head>
+<style>
+h1 {
+	text-align: center;
+	font-family: cursive;
+}
+h5{
+	text-align:center;
+	font-family:serif;
+
+}
+body {
+	background-image:url(grid_noise.png);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+}
+button{
+    align-items:center;
+}
+img{
+	display: flex;
+	align-items: center;
+	justify-content: center
+}
+</style>
+
+<title>Login Confirmation</title>
+</head>
+<body>
+<img src="success.jpeg" alt="e-librrr.jpg">
+<?php
+echo"<h1>You have successfully Login</h1>";
+echo"<h5>you can now Login with your Reg No and Password<h5>";
+?>
+
+
+<footer>
+ <button> <a href="index.php">Go to Home</h4></button>
+ </footer>
+</body>
+</html>
